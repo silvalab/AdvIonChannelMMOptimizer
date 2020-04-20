eaf499e5b4feb7360dcd1e9c01b572ea963767cb
mangoldk <57415474+mangoldk@users.noreply.github.com>
2020-04-20 10:58:14 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# NOTE: #PBS comments request resources and have to come before any commands
-
-# This option will send email when the job starts/finishes
-# To receive this email, you'll need a .forward file in your home
-# directory that contains the email address you wish to receive your
-# email at.
-
-# supposedly stop emailing me
-#PBS -m n
-# Here I request 1 node with 1 processors per node (ppn) for 1 hour of
-# walltime.
-#PBS -l nodes=1:ppn=1,walltime=24:00:00
 
 echo Your args are: "$@"
-echo $LD_LIBRARY_PATH
+#update the contents of the LD_LIBRARY_PATH to make sure the Intel MKL libraries are available. Ammend the following statement for your specific installation
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/intel/compilers_and_libraries_2017.6.256/linux/compiler/lib/intel64:/export/intel/compilers_and_libraries_2017.6.256/linux/mkl/lib/intel64
 echo $LD_LIBRARY_PATH
-echo $N
-echo $NUMBER
-echo $TIMES
-echo $VERSION
-ulimit -c unlimited
-ulimit -s unlimited
-if [ $# -eq 1 ] 
+
+#Standard Notation for calling the program is
+# ./Evalulator N(Number of states) Model_Number(ID of the model of within N to optimize) Times (initial starts) Version
+
+# Therefore to optimize the fully connected 3-state model (3rd listed model in State3/State3parsedDT4CLT4.txt) with 10 Sobol starts and save the results to folder "test"
+./Evalulator 3 3 10 test
+
+# To restart the optimization from the last saved progress point, append on restart
+./Evalulator 3 3 10 test restart
+
+if [ $# -eq 4 ] 
 then
 	echo "restart"
-	/home/mangoldk/GitHub/Evalulator /home/mangoldk/GitHub/solver.txt /home/mangoldk/GitHub/Master_Data2/protocolsWT.lst /home/mangoldk/GitHub/Master_Data2/validationWT.lst ${N} /home/mangoldk/GitHub/State${N}/State${N}parsedDT4CLT4.txt ${NUMBER} /home/mangoldk/GitHub/joe-kuo-other-4.5600 ${TIMES} ${VERSION} /home/mangoldk/GitHub/State${N}/${VERSION}/Model${NUMBER}/iter_state.txt
+	./Evalulator solver.txt Sample_Data/protocolsWT.lst Sample_Data/validationWT.lst $1 State$1/State$1parsedDT4CLT4.txt $2 joe-kuo-other-4.5600 $3 $4 State$1/$4/Model$2/iter_state.txt
 else
 	
-	/home/mangoldk/GitHub/Evalulator /home/mangoldk/GitHub/solver.txt /home/mangoldk/GitHub/Master_Data2/protocolsWT.lst /home/mangoldk/GitHub/Master_Data2/validationWT.lst ${N} /home/mangoldk/GitHub/State${N}/State${N}parsedDT4CLT4.txt ${NUMBER} /home/mangoldk/GitHub/joe-kuo-other-4.5600 ${TIMES} ${VERSION}
+	./Evalulator solver.txt Sample_Data/protocolsWT.lst Sample_Data/validationWT.lst $1 State$1/State$1parsedDT4CLT4.txt $2 joe-kuo-other-4.5600 $3 $4
 fi
