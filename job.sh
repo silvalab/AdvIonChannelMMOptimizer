8fd3e1296f58fd4fd359bb0763acf2358a2a045d
mangoldk <57415474+mangoldk@users.noreply.github.com>
2020-04-20 15:21:42 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -10,12 +10,12 @@ echo $LD_LIBRARY_PATH
 # ./Evalulator N(Number of states) Model_Number(ID of the model of within N to optimize) Times (initial starts) Version
 
 # Therefore to optimize the fully connected 3-state model (3rd listed model in State3/State3parsedDT4CLT4.txt) with 10 Sobol starts and save the results to folder "test"
-./Evalulator 3 3 10 test
+#./Evalulator 3 3 10 test
 
 # To restart the optimization from the last saved progress point, append on restart
-./Evalulator 3 3 10 test restart
+#./Evalulator 3 3 10 test restart
 
-if [ $# -eq 4 ] 
+if [ $# -eq 5 ] 
 then
 	echo "restart"
 	./Evalulator solver.txt Sample_Data/protocolsWT.lst Sample_Data/validationWT.lst $1 State$1/State$1parsedDT4CLT4.txt $2 joe-kuo-other-4.5600 $3 $4 State$1/$4/Model$2/iter_state.txt
